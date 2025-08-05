74715e047fed2c0df509be4980f1966c9ca48338
Achyut Tripathi <achyuttripathi06@gmail.com>
2025-08-05 22:40:00 +0530

    Time: 17 ms (73.69%) | Memory: 44.2 MB (36.04%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,29 +1,53 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         int n = nums.length;
-        HashSet<List<Integer>> set = new HashSet<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums); 
 
         for (int i = 0; i < n; i++) {
+            // avoid the duplicates while moving i
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
             for (int j = i + 1; j < n; j++) {
-                HashSet<Long> hashset = new HashSet<>();
-                for (int k = j + 1; k < n; k++) {
-                    long sum = nums[i] + nums[j];
+                // avoid duplicate for j
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                continue;
+                }
+
+                // 2 pointers
+                int k = j + 1;
+                int l = n - 1;
+                while (k < l) {
+                    long sum = nums[i];
+                    sum += nums[j];
                     sum += nums[k];
-                    long fourth = target - sum;
-                    if (hashset.contains(fourth)) {
+                    sum += nums[l];
+                    if (sum == target) {
                         List<Integer> temp = new ArrayList<>();
                         temp.add(nums[i]);
                         temp.add(nums[j]);
                         temp.add(nums[k]);
-                        temp.add((int) fourth);
-                        temp.sort(Integer::compareTo);
-                        set.add(temp);
+                        temp.add(nums[l]);
+                        ans.add(temp);
+                        k++;
+                        l--;
+
+                        // skip the duplicates
+                        while (k < l && nums[k] == nums[k - 1]) {
+                            k++;
+                        }
+                        while (k < l && nums[l] == nums[l + 1]) {
+                            l--;
+                        }
+                    } else if (sum < target){
+                        k++;
+                    } else {
+                        l--;
                     }
-                    hashset.add((long) nums[k]);
                 }
             }
         }
-        List<List<Integer>> ans = new ArrayList<>(set);
         return ans;
     }
 }
