79c028543408b07bb2f8b79d602d37a637c2cb83
Achyut Tripathi <achyuttripathi06@gmail.com>
2025-12-08 14:09:28 +0530

    Time: 1 ms (99.69%) | Memory: 43.3 MB (9.15%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,23 +1,13 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-
-        HashMap<Character, Integer> map = new HashMap<>();
-
-        // Count frequencies in s
-        for (char c : s.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
+        int sum = 0;
+        for (char c : s.toCharArray()){
+            sum -= c;
         }
 
-        // Subtract frequencies using t
-        for (char c : t.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) - 1);
-
-            // If count becomes negative, that is the extra character
-            if (map.get(c) < 0) {
-                return c;
-            }
+        for (char c : t.toCharArray()){
+            sum += c;
         }
-
-        return ' '; // never reached
+        return (char)sum;
     }
 }
