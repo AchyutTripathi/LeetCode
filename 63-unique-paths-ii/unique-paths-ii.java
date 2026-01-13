0a6c19846fbec6b1aa368e7e33a4794cc37f4599
Achyut Tripathi <achyuttripathi06@gmail.com>
2026-01-13 21:42:03 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (29.04%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,21 +1,21 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int dp[][] = new int[obstacleGrid.length][obstacleGrid[0].length];
+        int[][] dp = new int[obstacleGrid.length][obstacleGrid[0].length];
         for (int i = 0; i < obstacleGrid.length; i++) {
             Arrays.fill(dp[i], -1);
         }
-        return func(obstacleGrid, 0, 0, dp);
+        return backtrack(obstacleGrid, dp, 0, 0);
     }
-
-    private int func(int[][] obstacleGrid, int row, int col, int[][] dp) {
-        if (row >= obstacleGrid.length || col >= obstacleGrid[0].length || obstacleGrid[row][col] == 1) {
+    private int backtrack(int[][] obstacleGrid, int[][] dp, int r, int c) {
+        if (r >= obstacleGrid.length || c >= obstacleGrid[0].length || obstacleGrid[r][c] == 1) {
             return 0;
         }
-        if (row == obstacleGrid.length - 1 && col == obstacleGrid[0].length - 1) {
+        if (r == obstacleGrid.length - 1 && c == obstacleGrid[0].length - 1) {
             return 1;
         }
-        if (dp[row][col] != -1) return dp[row][col];
-        return dp[row][col] = func(obstacleGrid, row + 1, col, dp) + func(obstacleGrid, row, col + 1, dp);
+        if (dp[r][c] != -1) {
+            return dp[r][c];
+        }
+        return dp[r][c] = backtrack(obstacleGrid, dp, r + 1, c) + backtrack(obstacleGrid, dp, r, c + 1); 
     }
 }
-
