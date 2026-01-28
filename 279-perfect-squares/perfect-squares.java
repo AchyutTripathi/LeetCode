284c2ffebe291478b6195d599fbc58a0aa50d3d5
Achyut Tripathi <achyuttripathi06@gmail.com>
2026-01-28 12:11:06 +0530

    Time: 30 ms (76.38%) | Memory: 44.7 MB (48.44%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -1,18 +1,14 @@
 class Solution {
-    int[] memo;
     public int numSquares(int n) {
-        if (n == 0) return 0;
-        memo = new int [n + 1];
-        return solve(n);
-    }
-    private int solve(int n) {
-        if (n == 0) return 0;
-        int count = Integer.MAX_VALUE;
-        if (memo[n] != 0) return memo[n];
-        for (int i = 1; i * i <= n; i++) {
-            count = Math.min(count, 1 + solve(n - i * i));
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            dp[i] = Integer.MAX_VALUE;
+
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], 1 + dp[i - j * j]);
+            }
         }
-        memo[n] = count;
-        return count;
+        return dp[n];
     }
 }
